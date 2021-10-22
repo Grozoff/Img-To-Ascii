59c2144959728df400f7da2d6bf080e060a2ea1f
Sergei Grozov <grozov.sv@gmail.com>
2021-10-22 15:14:50 +0500

    Add project files.

---
+++ ImgToAscii/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace ImgToAscii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownQuality = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownWidthOffset = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.imageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label5Complete = new System.Windows.Forms.Label();
+            this.checkBoxInvert = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownQuality)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidthOffset)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.checkBoxInvert);
+            this.splitContainer1.Panel1.Controls.Add(this.label5Complete);
+            this.splitContainer1.Panel1.Controls.Add(this.progressBar1);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonConvert);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.numericUpDownQuality);
+            this.splitContainer1.Panel1.Controls.Add(this.numericUpDownWidthOffset);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 537);
+            this.splitContainer1.SplitterDistance = 161;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(26, 146);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(106, 23);
+            this.buttonConvert.TabIndex = 5;
+            this.buttonConvert.Text = "Convert to ASCII";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Quality";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Width offset";
+            // 
+            // numericUpDownQuality
+            // 
+            this.numericUpDownQuality.Location = new System.Drawing.Point(85, 63);
+            this.numericUpDownQuality.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownQuality.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownQuality.Name = "numericUpDownQuality";
+            this.numericUpDownQuality.Size = new System.Drawing.Size(65, 20);
+            this.numericUpDownQuality.TabIndex = 2;
+            this.numericUpDownQuality.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownWidthOffset
+            // 
+            this.numericUpDownWidthOffset.DecimalPlaces = 1;
+            this.numericUpDownWidthOffset.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDownWidthOffset.Location = new System.Drawing.Point(85, 37);
+            this.numericUpDownWidthOffset.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            65536});
+            this.numericUpDownWidthOffset.Minimum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            65536});
+            this.numericUpDownWidthOffset.Name = "numericUpDownWidthOffset";
+            this.numericUpDownWidthOffset.Size = new System.Drawing.Size(65, 20);
+            this.numericUpDownWidthOffset.TabIndex = 2;
+            this.numericUpDownWidthOffset.Value = new decimal(new int[] {
+            17,
+            0,
+            0,
+            65536});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Output picture settings";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(631, 533);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.imageToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // imageToolStripMenuItem
+            // 
+            this.imageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.imageToolStripMenuItem.Name = "imageToolStripMenuItem";
+            this.imageToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.imageToolStripMenuItem.Text = "Image";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Enabled = false;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save to text";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Images|*.bmp;*.png;*.jpg;*.jpeg;";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Text |*.txt";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(16, 235);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(26, 175);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(106, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 8;
+            this.progressBar1.Visible = false;
+            // 
+            // label5Complete
+            // 
+            this.label5Complete.AutoSize = true;
+            this.label5Complete.BackColor = System.Drawing.SystemColors.Control;
+            this.label5Complete.Location = new System.Drawing.Point(53, 180);
+            this.label5Complete.Name = "label5Complete";
+            this.label5Complete.Size = new System.Drawing.Size(0, 13);
+            this.label5Complete.TabIndex = 9;
+            // 
+            // checkBoxInvert
+            // 
+            this.checkBoxInvert.AutoSize = true;
+            this.checkBoxInvert.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBoxInvert.Location = new System.Drawing.Point(9, 89);
+            this.checkBoxInvert.Name = "checkBoxInvert";
+            this.checkBoxInvert.Size = new System.Drawing.Size(91, 17);
+            this.checkBoxInvert.TabIndex = 10;
+            this.checkBoxInvert.Text = "Invert color    ";
+            this.checkBoxInvert.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 561);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Text = "Img to ASCII";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownQuality)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidthOffset)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDownQuality;
+        private System.Windows.Forms.NumericUpDown numericUpDownWidthOffset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem imageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label5Complete;
+        private System.Windows.Forms.CheckBox checkBoxInvert;
+    }
+}
+
---
+++ ImgToAscii/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImgToAscii
+{
+    public partial class Form1 : Form
+    {
+        private static int _quality;
+        private static double _widthOffset;
+        private Bitmap _bitmap;
+        private static string _filename;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ImagePreprocessing(Bitmap bitmap)
+        {
+            bitmap = ResizeBitmap(bitmap);
+            bitmap.ToGrayScale();
+
+            var converter = new BitmapToAsciiConverter(bitmap);
+
+            var rows = CheckInvertColor(converter);
+
+            progressBar1.Maximum = rows.GetLength(0);
+            foreach (var row in rows)
+            {
+                richTextBox1.Text += new string(row) + Environment.NewLine;
+                progressBar1.Value++;
+            }
+            if (progressBar1.Value == rows.GetLength(0))
+            {
+                progressBar1.Value = 0;
+                label5Complete.Text = "Complete!";
+            }
+        }
+
+        private char[][] CheckInvertColor(BitmapToAsciiConverter converter)
+        {
+            if (checkBoxInvert.Checked)
+                return converter.Convert();
+            else
+                return converter.ConvertInvert();
+        }
+
+        private async void StartConvert()
+        {
+            if (_bitmap == null)
+                return;
+
+            buttonConvert.Enabled = false;
+            richTextBox1.Clear();
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+
+            label5Complete.Text = string.Empty;
+            progressBar1.Visible = true;
+
+            _quality = (int)numericUpDownQuality.Value * 100;
+            _widthOffset = (double)numericUpDownWidthOffset.Value;
+
+            await Task.Run(() =>
+            {
+                //Invoke(new Action(() => { ImagePreprocessing(_bitmap); }));
+
+                ImagePreprocessing(_bitmap);
+            });
+            saveToolStripMenuItem.Enabled = true;
+            buttonConvert.Enabled = true;
+            progressBar1.Visible = false;
+        }
+
+        private static Bitmap ResizeBitmap(Bitmap bitmap)
+        {
+            var newHeight = bitmap.Height / _widthOffset * _quality / bitmap.Width;
+
+            if (bitmap.Width > _quality || bitmap.Height > newHeight)
+                bitmap = new Bitmap(bitmap, new Size(_quality, (int)newHeight));
+
+            return bitmap;
+        }
+
+        private static void openSavedFileInExplorer(string path)
+        {
+            if (File.Exists(path))
+                Process.Start("explorer.exe", "/select, " + path);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartConvert();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                _bitmap = new Bitmap(openFileDialog1.FileName);
+                label4.Text = "Original image";
+                pictureBox1.Image = _bitmap;
+                _filename = Path.GetFileNameWithoutExtension(openFileDialog1.FileName) + " ASCII.txt";
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = _filename;
+            
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var fileName = saveFileDialog1.FileName;
+                File.WriteAllText(fileName, richTextBox1.Text);
+                saveFileDialog1.FileName = string.Empty;
+                openSavedFileInExplorer(fileName);
+            }
+        }
+    }
+}
